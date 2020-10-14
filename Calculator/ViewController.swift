d40e6eda7b7d0cf54ad56ca78543814af0a40a4e
Clerin <ccmatheckal@speedway.com>
2020-10-14 18:25:41 -0400

    removed space

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        displayLabel.text = "0.0   "
+        displayLabel.text = "0.0"
     }
     
     
